1f66101d353f98675dd38ee2d73516e3b4ec4088
Benjamin Mark <benjamin@okra.ng>
2019-11-24 16:13:44 +0100

    added few doc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 // @flow
 import axios from "axios";
 
-/**
- * This function says hello.
- * @param name Some name to say hello for.
- * @returns The hello.
- */
 const apiURL = "https://dev-api.okra.ng/v1/";
 
+/**
+ * getAuth - endpoint then allows to retrieve the bank account and
+ * routing numbers associated with a Record
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getAuth = function(
   token: string,
   options: { page: number, limit: number } | {},
@@ -28,6 +30,13 @@ export const getAuth = function(
   });
 };
 
+/**
+ * getTransactions - endpoint allows developers to receive customer-authorized
+ * transaction data for current, savings, and domiciliary Accounts
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getTransactions = function(
   token: string,
   options: { page: number, limit: number } | {},
@@ -48,6 +57,12 @@ export const getTransactions = function(
   });
 };
 
+/**
+ * getBalance - This endpoint returns the real-time balance for each of an Reocrd's accounts.
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getBalance = function(
   token: string,
   options: { page: number, limit: number } | {},
@@ -68,6 +83,13 @@ export const getBalance = function(
   });
 };
 
+/**
+ * getIdentity - This endpoint allows you to retrieve various account holder information
+ * on file
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getIdentity = function(
   token: string,
   options: { page: number, limit: number } | {},
@@ -88,6 +110,12 @@ export const getIdentity = function(
   });
 };
 
+/**
+ * getRecords - retrieve all records associated with the company
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getRecords = function(
   token: string,
   options: { page: number, limit: number } | {},
@@ -108,6 +136,12 @@ export const getRecords = function(
   });
 };
 
+/**
+ * getAccount - retrieve account(s) associated to company
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getAccount = function(
   token: string,
   options: { page: number, limit: number } | {},
@@ -128,6 +162,12 @@ export const getAccount = function(
   });
 };
 
+/**
+ * getProduct - retrieves the list of products available on an account
+ * @param {string} token - AccessToken
+ * @param {object} options - {page and limit}
+ * @param {function} callback
+ */
 export const getProducts = function(
   token: string,
   options: { page: number, limit: number } | {},
