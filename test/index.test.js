8c0974eb160b790ec1719a79a85b46f995236847
Benjamin Mark <benjamin@okra.ng>
2019-11-24 15:22:07 +0100

    added few tests on api methods

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,76 @@
-import sayHello from "../src";
+import * as api from "../src";
 
-test("sayHello", () => {
-  expect(sayHello()).toBe("Hello, Haz!");
-  expect(sayHello("foo")).toBe("Hello, foo!");
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZGMyZTU1MmM2NDNkODZlZDYyNGQ4YjEiLCJpYXQiOjE1NzQ2MDIxNDV9.kalMXY220arFY6YYiDrH5n4wqXDpfnLBIZog8YLDQxY";
+describe("Testing module", () => {
+  test("that it is defined", () => {
+    expect(api).toBeDefined();
+  });
+});
+
+describe("Testing getAuth Api", () => {
+  test("getAuth get response", async () => {
+    const response = await api.getAuth(token, { page: 1, limit: 1 }, () => {
+      return true;
+    });
+    expect(response).toBe(true);
+  });
+});
+
+describe("Testing getTransactions Api", () => {
+  test("getTransactions get response", async () => {
+    const response = await api.getTransactions(
+      token,
+      { page: 1, limit: 1 },
+      () => {
+        return true;
+      }
+    );
+    expect(response).toBeTruthy();
+  });
+});
+
+describe("Testing getBalance Api", () => {
+  test("getBalance get response", async () => {
+    const response = await api.getBalance(token, { page: 1, limit: 1 }, () => {
+      return true;
+    });
+    expect(response).toBeTruthy();
+  });
+});
+
+describe("Testing getIdentity Api", () => {
+  test("getIdentity get response", async () => {
+    const response = await api.getIdentity(token, { page: 1, limit: 1 }, () => {
+      return true;
+    });
+    expect(response).toBeTruthy();
+  });
+});
+
+describe("Testing getRecords Api", () => {
+  test("getRecords get response", async () => {
+    const response = await api.getRecords(token, { page: 1, limit: 1 }, () => {
+      return true;
+    });
+    expect(response).toBeTruthy();
+  });
+});
+
+describe("Testing getAccount Api", () => {
+  test("getAccount get response", async () => {
+    const response = await api.getAccount(token, { page: 1, limit: 1 }, () => {
+      return true;
+    });
+    expect(response).toBeTruthy();
+  });
+});
+
+describe("Testing getProducts Api", () => {
+  test("getProducts get response", async () => {
+    const response = await api.getProducts(token, { page: 1, limit: 1 }, () => {
+      return true;
+    });
+    expect(response).toBeTruthy();
+  });
 });
