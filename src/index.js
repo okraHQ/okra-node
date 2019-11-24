55d33268096243ddb2c5cd9645d5bd4d9b894e8a
Benjamin Mark <benjamin@okra.ng>
2019-11-24 15:17:43 +0100

    refactored and added few endpoint methods

--- src/index.js
+++ src/index.js
@@ -8,96 +8,152 @@ import axios from "axios";
  */
 const apiURL = "https://dev-api.okra.ng/v1/";
 
-interface IInput {
-  token: string;
-  options: { page: number, limit: number } | {};
-  callback: (error: string | null, result: [{}] | null) => mixed;
-}
+export const getAuth = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/auth/get`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.auths);
+    }
+    return callback(response.data.msg, null);
+  });
+};
 
-module.exports = {
-  getAuth(input: IInput) {
-    const url = `${apiURL}products/auth/get`;
-    const request = axios({
-      method: "POST",
-      url,
-      data: input.options,
-      headers: { Authorization: `Bearer ${input.token}` }
-    });
+export const getTransactions = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/transactions/get`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.trans);
+    }
+    return callback(response.data.msg, null);
+  });
+};
 
-    return request.then(response => {
-      if (response.data.status === "success") {
-        input.callback(null, response.data.data.auths);
-      } else {
-        input.callback(response.data.msg, null);
-      }
-    });
-  },
-  getTransactions(input: IInput) {
-    const url = `${apiURL}products/transactions/get`;
-    const request = axios({
-      method: "POST",
-      url,
-      data: input.options,
-      headers: { Authorization: `Bearer ${input.token}` }
-    });
+export const getBalance = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/balance/get`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.balances);
+    }
+    return callback(response.data.msg, null);
+  });
+};
 
-    return request.then(response => {
-      if (response.data.status === "success") {
-        input.callback(null, response.data.data.trans);
-      } else {
-        input.callback(response.data.msg, null);
-      }
-    });
-  },
-  getBalance(input: IInput) {
-    const url = `${apiURL}products/balance/get`;
-    const request = axios({
-      method: "POST",
-      url,
-      data: input.options,
-      headers: { Authorization: `Bearer ${input.token}` }
-    });
+export const getIdentity = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/identity/get`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.identities);
+    }
+    return callback(response.data.msg, null);
+  });
+};
 
-    return request.then(response => {
-      if (response.data.status === "success") {
-        input.callback(null, response.data.data.balances);
-      } else {
-        input.callback(response.data.msg, null);
-      }
-    });
-  },
-  getIdentity(input: IInput) {
-    const url = `${apiURL}products/identity/get`;
-    const request = axios({
-      method: "POST",
-      url,
-      data: input.options,
-      headers: { Authorization: `Bearer ${input.token}` }
-    });
+export const getRecords = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/records/get`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.records);
+    }
+    return callback(response.data.msg, null);
+  });
+};
 
-    return request.then(response => {
-      if (response.data.status === "success") {
-        input.callback(null, response.data.data.identities);
-      } else {
-        input.callback(response.data.msg, null);
-      }
-    });
-  },
-  getIncome(input: IInput) {
-    const url = `${apiURL}products/income/get`;
-    const request = axios({
-      method: "POST",
-      url,
-      data: input.options,
-      headers: { Authorization: `Bearer ${input.token}` }
-    });
+export const getAccount = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/accounts/get`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.accounts);
+    }
+    return callback(response.data.msg, null);
+  });
+};
 
-    return request.then(response => {
-      if (response.data.status === "success") {
-        input.callback(null, response.data.data.incomes);
-      } else {
-        input.callback(response.data.msg, null);
-      }
-    });
-  }
+export const getProducts = function(
+  token: string,
+  options: { page: number, limit: number } | {},
+  callback: (error: string | null, result: any) => mixed
+) {
+  const url = `${apiURL}products/list`;
+  const request = axios({
+    method: "POST",
+    url,
+    data: options,
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  return request.then(response => {
+    if (response.data.status === "success") {
+      return callback(null, response.data.data.products);
+    }
+    return callback(response.data.msg, null);
+  });
+};
+
+export default {
+  getAuth,
+  getTransactions,
+  getBalance,
+  getIdentity,
+  getRecords,
+  getAccount,
+  getProducts
 };
